54b91c69bb863a3fe9c55dabda8e355a957d8bcb
Gavin Simpson <ucfagls@gmail.com>
2011-09-20 22:52:47 +0000

    rescale needs to return Y and X components not U1 and U2

--- pkg/R/rescale.symcoca.R
+++ pkg/R/rescale.symcoca.R
@@ -1,14 +1,12 @@
-"rescale.symcoca" <-
-function(object, axes = c(1:object$n.axes), ...)
-  {
+"rescale.symcoca" <- function(object, axes = c(1:object$n.axes), ...) {
     col.nam <- colnames(object$scores$species$Y)[axes]
     lambda4 <- diag(sqrt(sqrt(object$lambda[axes])),
                     nrow = length(axes),
                     ncol = length(axes))
-    U1.scale <- object$scores$species$Y[, axes, drop = FALSE] %*% lambda4
-    U2.scale <- object$scores$species$X[, axes, drop = FALSE] %*% lambda4
-    colnames(U1.scale) <- colnames(U2.scale) <- col.nam
-    retval <- list(U1 = U1.scale, U2 = U2.scale)
+    Y.scale <- object$scores$species$Y[, axes, drop = FALSE] %*% lambda4
+    X.scale <- object$scores$species$X[, axes, drop = FALSE] %*% lambda4
+    colnames(Y.scale) <- colnames(X.scale) <- col.nam
+    retval <- list(Y = Y.scale, X = X.scale)
     return(retval)
-  }
+}
 
