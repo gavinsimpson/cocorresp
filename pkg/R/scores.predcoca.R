e9ccf6993db932226479a1ff2caf5be9e5da0202
Gavin Simpson <ucfagls@gmail.com>
2011-09-21 20:44:42 +0000

    check for inheritance not equivalence of class

--- pkg/R/scores.predcoca.R
+++ pkg/R/scores.predcoca.R
@@ -1,18 +1,17 @@
-"scores.predcoca" <-
-function(x, choices = c(1,2), display = c("sites", "species"), ...)
-  {
-    if (class(x) != "predcoca")
-      stop("x must be of class \"predcoca\"")
+`scores.predcoca` <- function(x, choices = c(1,2),
+                              display = c("sites", "species"), ...) {
+    if (!inherits(x, "predcoca"))
+        stop("x must be of class \"predcoca\"")
     scoreOpts <- c("species", "sites")
     names(scoreOpts) <- c("species", "sites")
     take <- scoreOpts[display]
     retval <- list()
     retval$species <- if ("species" %in% take)
-      list(U1 = x$scores$species$Y[, choices, drop = FALSE],
-           U2 = x$scores$species$X[, choices, drop = FALSE])
+        list(U1 = x$scores$species$Y[, choices, drop = FALSE],
+             U2 = x$scores$species$X[, choices, drop = FALSE])
     retval$site <- if ("sites" %in% take)
-      list(X1 = x$scores$site$Y[, choices, drop = FALSE],
-           X2 = x$scores$site$X[, choices, drop = FALSE])
+        list(X1 = x$scores$site$Y[, choices, drop = FALSE],
+             X2 = x$scores$site$X[, choices, drop = FALSE])
     class(retval) <- "scores.predcoca"
-    return(retval)
-  }
+    retval
+}
