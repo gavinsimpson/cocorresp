0265060adc5a76104ef71b72d3a4556eec3fdcc3
Gavin Simpson <ucfagls@gmail.com>
2011-09-21 20:51:39 +0000

    code tidy

--- pkg/R/corAxis.R
+++ pkg/R/corAxis.R
@@ -1,15 +1,13 @@
-"corAxis" <-
-function(x, ...)
-  UseMethod("corAxis")
+`corAxis` <- function(x, ...)
+    UseMethod("corAxis")
 
-"corAxis.default" <-
-function(x, ...)
-  stop("No default method for corAxis")
+`corAxis.default` <- function(x, ...)
+    stop("No default method for corAxis")
 
-"corAxis.symcoca" <-
-function(x, axes = c(1:min(6, x$n.axes)), ...) {
-  if (class(x) != "symcoca")
-    stop("object must be of class \"symcoca\"")
-  scrs <- scores(x, axes, display = "sites")
-  diag(cor(scrs$sites$Y, scrs$sites$X))
+`corAxis.symcoca` <- function(x, axes = c(1:min(6, x$n.axes)),
+                              ...) {
+    if (class(x) != "symcoca")
+        stop("object must be of class \"symcoca\"")
+    scrs <- scores(x, axes, display = "sites")
+    diag(cor(scrs$sites$Y, scrs$sites$X))
 }
