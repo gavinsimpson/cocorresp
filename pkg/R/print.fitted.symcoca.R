282bd47e467ec7cac89b8136294e559e8f513bbc
Gavin Simpson <ucfagls@gmail.com>
2013-12-11 04:11:34 +0000

    zap small numbers within machine precision

--- pkg/R/print.fitted.symcoca.R
+++ pkg/R/print.fitted.symcoca.R
@@ -2,8 +2,8 @@
 function(x, digits = max(3, getOption("digits") - 3), ...)
   {
     cat("\nFitted values for:", x$nam.dat["namY"], "\n")
-    print(x$Yhat1, digits = digits, ..., print.gap = 2)
+    print(zapsmall(x$Yhat1), digits = digits, ..., print.gap = 2)
     cat("\nFitted values for:", x$nam.dat["namX"], "\n")
-    print(x$Yhat2, digits = digits, ..., print.gap = 2)
+    print(zapsmall(x$Yhat2), digits = digits, ..., print.gap = 2)
   }
 
