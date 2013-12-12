7a2c6fe57643380164ae0661db33414df321ffdb
Gavin Simpson <ucfagls@gmail.com>
2013-12-12 03:20:43 +0000

    incorrectly determining class inheritance

--- pkg/R/corAxis.R
+++ pkg/R/corAxis.R
@@ -6,7 +6,7 @@
 
 `corAxis.symcoca` <- function(x, axes = c(1:min(6, x$n.axes)),
                               ...) {
-    if (class(x) != "symcoca")
+    if (!inherits(x, "symcoca"))
         stop("object must be of class \"symcoca\"")
     scrs <- scores(x, axes, display = "sites")
     diag(cor(scrs$sites$Y, scrs$sites$X))
