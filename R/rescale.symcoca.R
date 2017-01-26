43d1656bf3542f6f28d52f68390058fe07434e55
Gavin Simpson <ucfagls@gmail.com>
2017-01-25 23:36:53 -0600

    improvements to allow it to work more like scores

--- R/rescale.symcoca.R
+++ R/rescale.symcoca.R
@@ -1,12 +1,26 @@
-`rescale.symcoca` <- function(object, axes = seq_len(object$n.axes), ...) {
-    col.nam <- colnames(object$scores$species$Y)[axes]
-    lambda4 <- diag(sqrt(sqrt(object$lambda[axes])),
-                    nrow = length(axes),
-                    ncol = length(axes))
-    Y.scale <- object$scores$species$Y[, axes, drop = FALSE] %*% lambda4
-    X.scale <- object$scores$species$X[, axes, drop = FALSE] %*% lambda4
-    colnames(Y.scale) <- colnames(X.scale) <- col.nam
-    retval <- list(Y = Y.scale, X = X.scale)
-    retval
+`rescale.symcoca` <- function(object, choices = NULL,
+                              display = c("species", "sites"), ...) {
+    if (is.null(choices)) {
+        choices <- seq_len(object$n.axes)
+    }
+    display <- match.arg(display, several.ok = TRUE)
+    lambda4 <- diag(sqrt(sqrt(eigenvals(object, choices = choices))),
+                    nrow = length(choices),
+                    ncol = length(choices))
+    out <- list()
+    if ("species" %in% display) {
+        out$species <-
+            list(Y = object$scores$species$Y[, choices, drop = FALSE] %*% lambda4,
+                 X = object$scores$species$X[, choices, drop = FALSE] %*% lambda4)
+    }
+    if ("sites" %in% display) {
+        out$sites <-
+            list(Y = object$scores$site$Y[, choices, drop = FALSE] %*% lambda4,
+                 X = object$scores$site$X[, choices, drop = FALSE] %*% lambda4)
+    }
+    if (length(out) == 1L) {
+        out <- out[[1L]]
+    }
+    out
 }
 
