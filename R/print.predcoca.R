5ada3ea893dafec60cd6c27caa28673ef3f8ca46
Gavin Simpson <ucfagls@gmail.com>
2020-06-15 15:32:21 -0600

    The labels in the print methods for predcoca objects was wrongly labelling the data sets with the incorrect data object name

--- R/print.predcoca.R
+++ R/print.predcoca.R
@@ -16,7 +16,10 @@
         totVar <- data.frame(Role = c("Predictor", "Response"),
                              "Variance" = totVar,
                              check.names = FALSE, stringsAsFactors = FALSE)
-        rownames(totVar) <- unlist(x$nam.dat, use.names = FALSE)
+        ## x$totVar stored as Xblock (covariates), Yblock (response)
+        ## so extract the names as covariate and response.
+        ## actually stored in the other order x$nam.dat[1L] == response
+        rownames(totVar) <- c(x$nam.dat$namX, x$nam.dat$namY)
         print(totVar, digits = digits, ...)
     }
     invisible(x)
